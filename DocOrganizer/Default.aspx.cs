b9775d6ac3f1fb4b9d50fbeb204e2dd65fd53b32
lerwine <lenny@erwinefamily.net>
2014-01-23 07:14:20 -0500

    Initial Check-In

---
+++ DocOrganizer/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.EntityClient;
+
+namespace DocOrganizer
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EntityConnection ec = new EntityConnection();
+            ec.Open()
+        }
+    }
+}
